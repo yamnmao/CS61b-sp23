16d27b8df5ec1a41e8b80c51ba64daed4784e137
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-05-25 18:39:53 -0700

    proj0

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,6 +1,8 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,15 +22,41 @@ public class NaiveLetterFreqGuesser implements Guesser {
     /** Returns a map from a given letter to its frequency across all words.
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
-        // TODO: Fill in this method.
-        return null;
+        Map<Character, Integer> result = new HashMap<>();
+        int count =0;
+        //get word from words list
+        for(String word:words){
+            //The Java string toCharArray() method converts the given string into a sequence of characters.
+            //get character from the word
+            for(char c:word.toCharArray()){
+                char lowercaseLetter = Character.toLowerCase(c);
+                result.put(lowercaseLetter,result.getOrDefault(lowercaseLetter,0)+1);
+
+            }
+        }
+        return result;
     }
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
-        // TODO: Fill in this method.
-        return '?';
+        char common = '?';
+        int max = 0;
+        Map<Character, Integer> frequencyMap = getFrequencyMap();
+        for(char guess:guesses){
+            frequencyMap.remove(guess);
+        }
+        for(Map.Entry<Character,Integer>entry: frequencyMap.entrySet()){
+            char letter = entry.getKey();
+            int frequency = entry.getValue();
+
+            if (frequency > max) {
+                common = letter;
+                max = frequency;
+            }
+        }
+
+        return common;
     }
 
     public static void main(String[] args) {
--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -1,8 +1,11 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class PatternAwareLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -15,9 +18,73 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        // TODO: Fill in this method.
-        return '?';
+        //keepWords select words matching the pattern
+        List<String> nw = this.keepWords(pattern);
+        //get character frequency map from
+        Map<Character, Integer> freq=this.getFrequencyMap(nw);
+        char ch = '?';
+        int max=0;
+        for (char an: freq.keySet()) {
+            if (!guesses.contains(an)) {
+                if (max < freq.get(an)) {
+                    max = freq.get(an);
+                    ch = an;
+                }
+            }
+        }
+        return ch;
+
     }
+    public List<String> keepWords(String pattern){
+        Map<Character,Integer> map = new TreeMap<>();
+        List<String> copy = new ArrayList<>(words);
+        int index=0;
+        //iterate through the pattern, collect the character and respective indices
+        for (char ch: pattern.toCharArray()){
+            //if the ch not equal to -, then put it to the map
+            if (!(ch=='-')){
+                map.put(ch,index);
+            }
+            index++;
+            /*for example pattern is --e-(0,1,2,3), the first two -- will skip if statement
+            and index is increased to 2, when it meets "e", it will jump into
+            if statement and put the key-char ch, and the value-index 2 into the map
+             */
+        }
+        //iterate through the words (dictionary)
+        for (String word: words){
+            //iterate through the key(character from pattern) of the map
+            for (char ab: map.keySet()){
+                //remove the word if the word doesn't have the char at the specific indice
+                /*map.get(ab)=ab's indice, this map<key=char, value=indice>
+                whether word.charAt(ab's indice) == ab(the char in the map)
+                remove the word if it doesn't contain the char with indice.
+                 */
+                if (!(word.charAt(map.get(ab))==ab)){
+                    copy.remove(word);
+                }
+            }
+        }
+        return copy;
+    }
+
+    public Map<Character, Integer> getFrequencyMap(List<String> lst) {
+        Map<Character, Integer> map = new TreeMap<>();
+        for (String word : lst) {
+            for (char c : word.toCharArray()) {
+                if (map.containsKey(c)) {
+                    int oc = map.get(c);
+                    map.put(c, oc + 1);
+                } else {
+                    map.put(c, 1);
+                }
+               /* char lowerCase = Character.toLowerCase(c);
+                map.put(lowerCase,map.getOrDefault(lowerCase,0)+1);*/
+            }
+        }
+        return map;
+    }
+
 
     public static void main(String[] args) {
         PatternAwareLetterFreqGuesser palfg = new PatternAwareLetterFreqGuesser("data/example.txt");
