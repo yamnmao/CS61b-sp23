74515ea69ae5aa01f8c41c04d290761e36db7954
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-05-25 17:34:00 -0700

    Resolve conflicts

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,25 +1,54 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        return 0;
+        int totalSum =0;
+        for(int num:L){
+            totalSum += num;
+        }
+        return totalSum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List<Integer> evens = new ArrayList<Integer>();
+        for(int num:L){
+            if (num%2==0){
+                evens.add(num);
+            }
+        }
+        return evens;
+
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> commonList = new ArrayList<>();
+        for(int num:L1){
+            if (L2.contains(num)) {
+                commonList.add(num);
+            }
+        }
+        return commonList;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        return 0;
+        int count = 0;
+        //The outer loop (for (String word : words))iterates through each word in the list words.
+        for(String word:words){
+            //For each word, the inner loop (for (char ch : word.toCharArray()))iterates through each character
+            //The Java string toCharArray() method converts the given string into a sequence of characters.
+            for(char check : word.toCharArray()){
+                if(check == c){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,23 +1,48 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
+      /*letterToNum: This method returns a map from every lower case letter
+    to the number corresponding to that letter starting with ‘a’ is 1.
+     */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+
+        Map<Character, Integer> result = new HashMap<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            int num = c - 'a' + 1;
+            result.put(c, num);
+        }
+        return result;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        Map<Integer,Integer> result = new HashMap<>();
+        int squares = 0;
+        if(nums.isEmpty()==true){
+            System.out.print("empty");
+            return result;
+        }else {
+            for (int num : nums) {
+                result.put(num, num*num);
+            }
+            return result;
+        }
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+        Map<String,Integer>result = new HashMap<>();
+        for (String word : words){
+            result.put(word,result.getOrDefault(word,0)+1);
+        }
+        return result;
     }
 }
